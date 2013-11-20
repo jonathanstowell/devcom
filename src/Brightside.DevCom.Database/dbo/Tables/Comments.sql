fc835ac5262267b051cba334152ff0c9475f5be0
Jonathan <stowell.jonathan@gmail.com>
2013-11-20 08:47:19 +0000

    Database Project.

---
+++ src/Brightside.DevCom.Database/dbo/Tables/Comments.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Comments] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL,
+    [PostId]  INT            NOT NULL,
+    [Author]  NVARCHAR (255) NOT NULL,
+    [Content] TEXT           NOT NULL,
+    CONSTRAINT [PK_Comments] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Comments_Posts] FOREIGN KEY ([PostId]) REFERENCES [dbo].[Posts] ([Id])
+);
+
---
+++ src/Brightside.DevCom.Database/dbo/Tables/Posts.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Posts] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL,
+    [Author]  NVARCHAR (255) NOT NULL,
+    [Content] TEXT           NOT NULL,
+    CONSTRAINT [PK_Posts] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
