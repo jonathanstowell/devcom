1f1f89ad5533f04527a9b5f389a848ced0ef25eb
Jonathan <stowell.jonathan@gmail.com>
2013-11-20 10:37:31 +0000

    composed knockout.

---
+++ src/Brightside.DevCom.Posts.WebUI/js/models/posts/details.model.js
@@ -0,0 +1,70 @@
+﻿define(["jquery", "knockout", "kovalidation", "signalr.hubs", "bootstrap"], function($, ko) {
+    var index = {};
+
+    index.loading = ko.observable(true);
+    index.error = ko.observable(false);
+    index.editing = ko.observable(false);
+
+    index.id = ko.observable();
+    index.author = ko.observable('').extend({
+        required: {
+            message: "'Author' should not be empty."
+        }
+    });
+
+    index.content = ko.observable('').extend({
+        required: {
+            message: "'Content' should not be empty."
+        }
+    });
+
+    index.comments = ko.observableArray([]);
+
+    index.edit = function() {
+        index.editing(!index.editing());
+    };
+
+    index.save = function() {
+        if (!ko.validation.isValid(index)) {
+            return;
+        }
+
+        $.ajax({
+            type: "PUT",
+            url: "api/posts/",
+            data: { Id: index.id(), Author: index.author(), Content: index.content() }
+        }).done(function(response) {
+            index.editing(!index.editing());
+            $(document).trigger('postUpdated', [response]);
+        })
+            .fail(function(request, status, error) {
+                if (request.status == 400) {
+                    var validationResult = $.parseJSON(request.responseText);
+                    ko.validation.rebindValidations(index, validationResult.Results.Errors);
+                }
+
+                if (request.status == 500) {
+                    alert("internal server");
+                }
+            });
+    };
+
+    $(document).bind('loadPostDetails', function(e, id) {
+        $.getJSON("api/posts/" + id, function(response) {
+            index.id(response.id);
+            index.author(response.author);
+            index.content(response.content);
+            index.comments(response.comments);
+
+            index.editing(false);
+
+            index.formModal.modal('show');
+        });
+    });
+
+    $(function() {
+        index.formModal = $('#post-details-modal').modal({ backdrop: true, closeOnEscape: true, modal: true, show: false });
+    });
+
+    return index;
+});
---
+++ src/Brightside.DevCom.Posts.WebUI/js/viewmodels/posts/index.viewmodel.js
@@ -0,0 +1,19 @@
+﻿requirejs(["jquery",
+        "knockout",
+        "mylibs/plugins/models/posts/list.model",
+        "mylibs/plugins/models/posts/create.model",
+        "mylibs/plugins/models/posts/details.model",
+        "kovalidation",
+        "signalr.hubs",
+        "bootstrap"], function ($, ko, listModel, createModel, detailsModel) {
+            
+    var model = {
+        list: listModel,
+        create: createModel,
+        details: detailsModel
+    };
+
+    $(function () {
+        ko.applyBindings(model, jQuery('#posts')[0]);
+    });
+});
